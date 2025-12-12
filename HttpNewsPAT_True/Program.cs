9915ff5d8edb0761911f4702d743b47e4c2b2ae7
student-a502 <student-a502@STUD-RM3-A502>
2025-12-12 14:59:55 +0500

    Fix

---
+++ HttpNewsPAT_True/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpNewsPAT_True
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("");
+        }
+    }
+}
