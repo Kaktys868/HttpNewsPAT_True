d07e8b0102a3a838c64c04ff78b173ce9a8f6eb5
Kaktys868 <lastochka1230@yandex.ru>
2025-12-15 21:10:36 +0500

    Создание метода GetContent. True

--- HttpNewsPAT_True/Program.cs
+++ HttpNewsPAT_True/Program.cs
@@ -12,21 +12,37 @@ namespace HttpNewsPAT_True
 {
     public class Program
     {
+        public static string url = "";
         static void Main(string[] args)
         {
-            SingIn("student", "Asdfg123");
+            CookieContainer cookieContainer = SingIn("student", "Asdfg123");
+
+            Uri uri = new Uri(url);
+            CookieCollection allCookies = cookieContainer.GetCookies(uri);
+
+            Console.WriteLine("\n=== ВСЕ ПОЛУЧЕННЫЕ КУКИ ===");
+            foreach (Cookie cookie in allCookies)
+            {
+                Console.WriteLine($"Имя: {cookie.Name}");
+                Console.WriteLine($"Значение: {cookie.Value}");
+                Console.WriteLine($"Домен: {cookie.Domain}");
+                Console.WriteLine($"Путь: {cookie.Path}");
+                Console.WriteLine("---");
+            }
+
+            GetContent(cookieContainer);
 
             Console.Read();
         }
-        
+
         public static CookieContainer SingIn(string Login, string Password)
         {
-            string url = "";
             Debug.WriteLine($"Выполняем запрос: {url}");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = new CookieContainer();
+
             string postData = $"login={Login}&password={Password}";
             byte[] Data = Encoding.ASCII.GetBytes(postData);
             request.ContentLength = Data.Length;
@@ -34,11 +50,27 @@ namespace HttpNewsPAT_True
             using (var stream = request.GetRequestStream())
                 stream.Write(Data, 0, Data.Length);
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Debug.WriteLine($"Статус выполннения: {response.StatusCode}");
-            string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            return request.CookieContainer;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+                string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(responseFromServer);
+                return request.CookieContainer;
+            }
         }
-    } 
+        public static string GetContent(CookieContainer cookieContainer)
+        {
+            Debug.WriteLine($"Выполняем запрос: {url}");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.CookieContainer = cookieContainer;  // Используем весь контейнер
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+                string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine("Ответ: " + responseFromServer);
+                return responseFromServer;
+            }
+        }
+    }
 }
